83889614854a907dd62a357fb99303f0cda20180
Senua <chc0089@gmail.com>
2025-11-29 17:17:08 +0000

    Initial Commit

---
+++ library-senua/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright <2025> Senua - email
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Library Senua",
+    "summary": "Módulo Gestión de Biblioteca",
+    "version": "18.0.1.0.0",
+    "category": "Library",
+    "website": "https://aeodoo.org",
+    "author": "aeodoo, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "data": [
+        
+    ],
+}
