78852e39005a88287b9b408795172e7484dd8fb6
Senua <chc0089@gmail.com>
2025-12-02 18:20:50 +0000

    Added Test to avoid to loan a book twice

--- library_senua/tests/__init__.py
+++ library_senua/tests/__init__.py
@@ -1,5 +1,6 @@
 from . import test_isbn_unique
 from . import test_return_late
+from . import test_cannot_loan_book_twice
 
 
 
---
+++ library_senua/tests/test_cannot_loan_book_twice.py
@@ -0,0 +1,39 @@
+from datetime import date
+from odoo.tests.common import TransactionCase
+from odoo.exceptions import ValidationError
+
+
+class TestCannotLoanBookTwice(TransactionCase):
+    
+    def test_cannot_loan_book_already_loaned(self):
+
+        # Create author and borrower partners
+        autor = self.env['res.partner'].create({'name': 'Author'})
+        borrower_one = self.env['res.partner'].create({'name': 'Borrower 1'})
+        borrower_two = self.env['res.partner'].create({'name': 'Borrower 2'})
+        
+        # Create a book
+        book = self.env['library.book'].create({
+            'name': 'Book One',
+            'isbn': '123-4567890123',
+            'author_id': autor.id,
+        })
+        
+        # First loan
+        loan1 = self.env['library.loan'].create({
+            'book_id': book.id,
+            'borrower_id': borrower_one.id,
+            'loan_date': date.today(),
+        })
+        
+        print(f"\n First loan created: {loan1.id} (state: {loan1.state})")
+        
+        # Attempt second loan of the same book - should fail
+        with self.assertRaises(ValidationError) as e:
+            loan2 = self.env['library.loan'].create({
+                'book_id': book.id,
+                'borrower_id': borrower_two.id,
+                'loan_date': date.today(),
+            })
+        
+        print(f" Error captured: {e.exception}")
